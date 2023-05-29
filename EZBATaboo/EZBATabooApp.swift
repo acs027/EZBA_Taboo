759183de003182a4cd44122fc372f60599707581
ali cihan <63219511+acs027@users.noreply.github.com>
2023-05-29 16:25:52 +0300

    Initial Commit

---
+++ EZBATaboo/EZBATabooApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZBATabooApp.swift
+//  EZBATaboo
+//
+//  Created by ali cihan on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EZBATabooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
